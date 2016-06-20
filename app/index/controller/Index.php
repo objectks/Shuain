38404e3eb43c93c616d66579eae895f7cc7ac0eb
Administrator <Administrator@KangShuai>
2016-06-20 09:27:47 +0800

    更新文件

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -1,13 +1,17 @@
 <?php
 namespace app\index\controller;
-import('ORG.Util.Page');
+
 use think\Controller;
+use think\Cache;
 
 class Index extends Controller
 {
 
     public function index()
     {       
+        //Cache::set('name',"KangShuai",3600);
+        //sleep(1);
+        dump(Cache::get('lijinhe')); // 获取缓存数据
         return $this->fetch();
     }
 }
