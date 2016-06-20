b4199b66ed8938a32ccdf4fa7777141bf8269817
Administrator <Administrator@KangShuai>
2016-06-20 09:28:28 +0800

    更新文件

--- public/index.php
+++ public/index.php
@@ -3,9 +3,7 @@
 // | ThinkPHP [ WE CAN DO IT JUST THINK ]
 // +----------------------------------------------------------------------
 // | Copyright (c) 2006-2015 http://thinkphp.cn All rights reserved.
-// +-----------------------
-
------------------------------------------------
+// +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
 // | Author: liu21st <liu21st@gmail.com>
@@ -14,8 +12,10 @@
 // [ 应用入口文件 ]
 
 // 定义应用目录
-define('APP_PATH', __DIR__ . '/../shuain/');
+define('APP_PATH', __DIR__ . '/../app/');
 // 开启调试模式
 define('APP_DEBUG', true);
+// 缓存路径
+define('CACHE_PATH', __DIR__.'/../extend/');
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
